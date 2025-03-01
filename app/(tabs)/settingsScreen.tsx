b8745cef908fbc5d885d65b3f4409c6dec08c528
CCheukKa <ccheukka@gmail.com>
2025-03-01 20:33:33 +0800

    refactor screen names

--- app/(tabs)/settingsScreen.tsx
+++ app/(tabs)/settingsScreen.tsx
@@ -7,7 +7,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function TabTwoScreen() {
+export default function SettingsScreen() {
     return (
         <ParallaxScrollView
             headerBackgroundColour={{ light: '#D0D0D0', dark: '#353636' }}
