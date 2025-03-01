097b106314788d3b38335a196397b6c40245db3d
CCheukKa <ccheukka@gmail.com>
2025-03-02 03:08:23 +0800

    make tab bar follow colour theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,37 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { useColourScheme } from '@/hooks/useColourScheme';
-import { ThemeColours } from '@/constants/ThemeColours';
 import { MaterialIcons } from '@expo/vector-icons';
+import { ThemeColours } from '@/constants/ThemeColours';
+import { ComponentProps } from 'react';
+import { StyleSheet, View } from 'react-native';
+
 
 export default function TabLayout() {
-    const colourScheme = useColourScheme();
+    const inactiveColour = ThemeColours.lowContrast;
+    const activeColour = ThemeColours.highContrast;
+
+    const tabBarIcon = (name: ComponentProps<typeof MaterialIcons>['name'], focused: boolean) => {
+        return <MaterialIcons
+            name={name}
+            size={28}
+            color={focused ? activeColour : inactiveColour}
+        />;
+    };
 
     return (
         <Tabs
             initialRouteName='clockScreen' //? This doesn't seem to work
             screenOptions={{
-                tabBarActiveTintColor: ThemeColours.highContrast,
                 headerShown: false,
-                tabBarButton: HapticTab,
-                tabBarBackground: TabBarBackground,
-                tabBarStyle: Platform.select({
-                    ios: {
-                        // Use a transparent background on iOS to show the blur effect
-                        position: 'absolute',
-                    },
-                    default: {},
-                }),
+                tabBarButton: HapticTab, //TODO: implement one that looks better
+                tabBarActiveTintColor: activeColour,
+                tabBarInactiveTintColor: inactiveColour,
+                tabBarActiveBackgroundColor: ThemeColours.background,
+                tabBarInactiveBackgroundColor: ThemeColours.background,
+                tabBarLabelStyle: {
+                    fontWeight: 'bold',
+                },
             }}>
 
             <Tabs.Screen
@@ -40,29 +46,31 @@ export default function TabLayout() {
                 name="infoScreen"
                 options={{
                     title: 'Info',
-                    tabBarIcon: ({ color: colour }) => (
-                        <MaterialIcons name="info-outline" size={28} color={colour} />
-                    ),
+                    tabBarIcon: ({ focused }) => tabBarIcon('info-outline', focused),
                 }}
             />
             <Tabs.Screen
                 name="clockScreen"
                 options={{
                     title: 'Clock',
-                    tabBarIcon: ({ color: colour }) => (
-                        <MaterialIcons name="access-time" size={28} color={colour} />
-                    ),
+                    tabBarIcon: ({ focused }) => tabBarIcon('access-time', focused),
                 }}
             />
             <Tabs.Screen
                 name="settingsScreen"
                 options={{
                     title: 'Settings',
-                    tabBarIcon: ({ color: colour }) => (
-                        <MaterialIcons name="settings" size={28} color={colour} />
-                    ),
+                    tabBarIcon: ({ focused }) => tabBarIcon('settings', focused),
                 }}
             />
         </Tabs>
     );
 }
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
