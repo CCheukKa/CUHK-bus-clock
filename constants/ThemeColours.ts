10a17be70c891e330d3b926dd9b72c83d9d0be2c
CCheukKa <ccheukka@gmail.com>
2025-03-02 01:39:09 +0800

    implement light theme

--- constants/ThemeColours.ts
+++ constants/ThemeColours.ts
@@ -1,23 +1,27 @@
 // const theme = useColourScheme() ?? 'light';
-const theme = 'dark';
+const theme = 'light';
 
 export const ThemeColours = {
-    // light: {
-    //     text: '#11181C',
-    //     background: '#ffffff',
-    //     black: '#000000',
-    //     tint: '#0a7ea4',
-    // },
+    light: {
+        black: '#000000',
+        white: '#ffffff',
+        //
+        text: '#11181C',
+        background: '#ffffff',
+        highContrast: '#000000',
+        halfContrast: '#606060',
+        lowContrast: '#a0a0a0',
+        dimContrast: '#c0c0c0',
+    },
     dark: {
         black: '#000000',
         white: '#ffffff',
         //
-        text: '#ECEDEE',
+        text: '#ecedee',
         background: '#151718',
         highContrast: '#ffffff',
         halfContrast: '#a0a0a0',
         lowContrast: '#606060',
         dimContrast: '#404040',
-        // accent: '#0a7ea4',
     }
 }[theme];
