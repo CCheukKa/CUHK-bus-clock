11530852d44c31037a4bea5f5b801cdda715da1e
CCheukKa <ccheukka@gmail.com>
2025-03-03 07:24:56 +0800

    feat: integrate react-native-paper for UI components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,31 @@
-import { Tabs } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Tabs } from 'expo-router';
 import { HapticTab } from '@/components/HapticTab';
 import { MaterialIcons } from '@expo/vector-icons';
 import { ThemeColours } from '@/constants/ThemeColours';
-import { ComponentProps } from 'react';
+import { ComponentProps, useEffect } from 'react';
 import { SettingsProvider } from '@/context/SettingsContext';
 import { StatusBar } from 'expo-status-bar';
+import { PaperProvider } from 'react-native-paper';
+import 'react-native-reanimated';
 
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+    /* -------------------------------------------------------------------------- */
+
     const inactiveColour = ThemeColours.lowContrast;
     const activeColour = ThemeColours.highContrast;
 
@@ -21,50 +40,52 @@ export default function RootLayout() {
     return (
         <>
             <StatusBar style="auto" />
-            <SettingsProvider>
-                <Tabs
-                    screenOptions={{
-                        headerShown: false,
-                        tabBarButton: HapticTab, //TODO: implement one that looks better
-                        tabBarActiveTintColor: activeColour,
-                        tabBarInactiveTintColor: inactiveColour,
-                        tabBarActiveBackgroundColor: ThemeColours.background,
-                        tabBarInactiveBackgroundColor: ThemeColours.background,
-                        tabBarLabelStyle: {
-                            fontWeight: 'bold',
-                        },
-                    }}>
+            <PaperProvider>
+                <SettingsProvider>
+                    <Tabs
+                        screenOptions={{
+                            headerShown: false,
+                            tabBarButton: HapticTab, //TODO: implement one that looks better
+                            tabBarActiveTintColor: activeColour,
+                            tabBarInactiveTintColor: inactiveColour,
+                            tabBarActiveBackgroundColor: ThemeColours.background,
+                            tabBarInactiveBackgroundColor: ThemeColours.background,
+                            tabBarLabelStyle: {
+                                fontWeight: 'bold',
+                            },
+                        }}>
 
-                    <Tabs.Screen
-                        name="index"
-                        options={{
-                            href: null,
-                        }}
-                    />
+                        <Tabs.Screen
+                            name="index"
+                            options={{
+                                href: null,
+                            }}
+                        />
 
-                    <Tabs.Screen
-                        name="infoScreen"
-                        options={{
-                            title: 'Info',
-                            tabBarIcon: ({ focused }) => tabBarIcon('info-outline', focused),
-                        }}
-                    />
-                    <Tabs.Screen
-                        name="clockScreen"
-                        options={{
-                            title: 'Clock',
-                            tabBarIcon: ({ focused }) => tabBarIcon('access-time', focused),
-                        }}
-                    />
-                    <Tabs.Screen
-                        name="settingsScreen"
-                        options={{
-                            title: 'Settings',
-                            tabBarIcon: ({ focused }) => tabBarIcon('settings', focused),
-                        }}
-                    />
-                </Tabs>
-            </SettingsProvider>
+                        <Tabs.Screen
+                            name="infoScreen"
+                            options={{
+                                title: 'Info',
+                                tabBarIcon: ({ focused }) => tabBarIcon('info-outline', focused),
+                            }}
+                        />
+                        <Tabs.Screen
+                            name="clockScreen"
+                            options={{
+                                title: 'Clock',
+                                tabBarIcon: ({ focused }) => tabBarIcon('access-time', focused),
+                            }}
+                        />
+                        <Tabs.Screen
+                            name="settingsScreen"
+                            options={{
+                                title: 'Settings',
+                                tabBarIcon: ({ focused }) => tabBarIcon('settings', focused),
+                            }}
+                        />
+                    </Tabs>
+                </SettingsProvider>
+            </PaperProvider>
         </>
     );
 }
