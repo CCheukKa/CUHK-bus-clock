05500a90aa1c777cfa89af54d8a5a6e5b01abd49
CCheukKa <ccheukka@gmail.com>
2025-03-02 20:35:42 +0800

    fix typo

--- hooks/useColourScheme.web.ts
+++ hooks/useColourScheme.web.ts
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
-import { useColorScheme as useRNColorScheme } from 'react-native';
+import { useColorScheme as useRNColourScheme } from 'react-native';
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
-export function useColorScheme() {
-  const [hasHydrated, setHasHydrated] = useState(false);
+export function useColourScheme() {
+    const [hasHydrated, setHasHydrated] = useState(false);
 
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
+    useEffect(() => {
+        setHasHydrated(true);
+    }, []);
 
-  const colorScheme = useRNColorScheme();
+    const colourScheme = useRNColourScheme();
 
-  if (hasHydrated) {
-    return colorScheme;
-  }
+    if (hasHydrated) {
+        return colourScheme;
+    }
 
-  return 'light';
+    return 'light';
 }
